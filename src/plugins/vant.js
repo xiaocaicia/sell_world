e83bdf7548c1821c583a3869427ca0a4e9640eb8
xujing <754130935@qq.com>
2020-01-03 21:08:10 +0800

    徐静第一次提交

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -2,7 +2,25 @@
 import Vue from 'vue'
 
 // 导入 Button 组件
-import { Button } from 'vant'
+import { 
+    Button,
+    Tabs,
+    Tab,
+    Grid,
+    GridItem,
+    Image,
+    ImagePreview,
+    Lazyload 
+ } from 'vant'
 
 // 挂载 Button 组件
 Vue.use(Button)
+Vue.use(Tabs)
+Vue.use(Tab)
+Vue.use(Grid)
+Vue.use(GridItem)
+Vue.use(Image)
+Vue.use(ImagePreview)
+Vue.use(Lazyload, {
+    lazyComponent: true
+  });
